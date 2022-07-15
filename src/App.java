ad15d12b31a8a78ee88e72edffc892df917e0224
Clebson Moura <clebson.sh@gmail.com>
2022-07-15 19:42:54 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        var KEY = "Your IMDB api key here!";
+
+        var url = "https://imdb-api.com/en/API/Top250Movies/" + KEY;
+        URI uri = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(uri).build();
+
+        String json = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+
+        System.out.println(json);
+    }
+}
